ebce123bc7d298f08a221e09b617057f38bb6778
wnl <zhichao.mai@se.com>
2020-05-29 13:30:12 +0800

    Prune console.log

--- server/interpolate-html.js
+++ server/interpolate-html.js
@@ -1,7 +1,6 @@
 const escapeStringRegexp = require("escape-string-regexp");
 
 const interpolateHtml = (content, env) => {
-  console.log("interpolateHtml -> env", env);
   Object.keys(env).forEach(key => {
     const value = env[key];
     content = content.replace(
