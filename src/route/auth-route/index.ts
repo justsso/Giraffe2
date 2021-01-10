4d914915dec01c56d6d48c91d4a1c06ebb9d0ea5
justsso <528226249@qq.com>
2021-01-10 16:37:07 +0800

    Add mock buildings and buildingData api

--- src/route/auth-route/index.ts
+++ src/route/auth-route/index.ts
@@ -137,4 +137,106 @@ export default function authRoute(app: Application) {
     });
     return res.sendStatus(200);
   });
+
+  // TODO: 将接口数据转换掉
+  app.get("/api/buildings", (req: Request, res: Response) => {
+    const buildings = [
+      {
+        id: 1,
+        buildingImg: "/assets/img/pic_bg.png",
+        buildingName: "建筑名称",
+        distributionBoxNum: 123,
+        alarmCount: 12
+      },
+      {
+        id: 2,
+        buildingImg: "/assets/img/pic_bg.png",
+        buildingName: "建筑名称2",
+        distributionBoxNum: 76,
+        alarmCount: 8
+      }
+      // {
+      //   id: 3,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称3",
+      //   distributionBoxNum: 3,
+      //   alarmCount: 0
+      // },
+      // {
+      //   id: 4,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称",
+      //   distributionBoxNum: 123,
+      //   alarmCount: 12
+      // },
+      // {
+      //   id: 5,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称2",
+      //   distributionBoxNum: 76,
+      //   alarmCount: 8
+      // },
+      // {
+      //   id: 6,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称3",
+      //   distributionBoxNum: 3,
+      //   alarmCount: 0
+      // },
+      // {
+      //   id: 7,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称",
+      //   distributionBoxNum: 123,
+      //   alarmCount: 12
+      // },
+      // {
+      //   id: 8,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称2",
+      //   distributionBoxNum: 76,
+      //   alarmCount: 8
+      // },
+      // {
+      //   id: 9,
+      //   buildingImg: "/assets/img/pic_bg.png",
+      //   buildingName: "建筑名称3",
+      //   distributionBoxNum: 3,
+      //   alarmCount: 0
+      // }
+    ];
+    res.send(buildings);
+  });
+
+  app.post(`/api/building/:buildingId`, (req: Request, res: Response) => {
+    const buildingData = {
+      id: 0,
+      parentId: 0,
+      name: "宝星园小区",
+      code: "string",
+      type: 0,
+      buildingArea: 0,
+      industryId: 0,
+      finishingDate: "2021-01-09T08:01:06.282Z",
+      subType: 0,
+      logoKey: "string",
+      buildingImg:
+        "https://se-test-static.energymost.com/to/module/Pop-UI/v8.0.5347/login-bg.90c8ff1f.jpg",
+      buildingName: "宝星园小区",
+      distributionBox: 98,
+      location: {
+        buildingId: 0,
+        latitude: 0,
+        longitude: 0,
+        province: "string",
+        districts: ["string"]
+      },
+      customerId: 0,
+      customerName: "string",
+      riskFactor: 0,
+      status: 0,
+      isLogbook: 0
+    };
+    res.send(buildingData);
+  });
 }
