ac22cc53dfcbc63f1d9b988d401402e7946e7c8e
justsso <claire.ji@se.com>
2021-01-12 16:05:19 +0800

    update .fitingnore

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 const {
   GIRAFFE_SESSION_SECRET = "P@ssw0rd",
   GIRAFFE_SESSION_MAX_AGE = 10000,
-  GIRAFFE_RABBITMQ_URL = "amqp://user1:password@172.26.66.4",
+  GIRAFFE_RABBITMQ_URL = "amqp://giraffe:password@pft-mq-l.hz.ds.se.com/giraffe",
   GIRAFFE_REDIS_SERVER = "r-bp15cb1344506784.redis.rds.aliyuncs.com",
   GIRAFFE_REDIS_PORT = 6379,
   GIRAFFE_REDIS_PWD = "NmP6HpG5N7gkY",
-  WXAPP_EPC_APP_ID = "wxc75fa6b7779cae2c,wxbbdb916ee0ef18aa",
-  WXAPP_EPC_APP_SECRET = "44419507dcaabf364b139b225482958e,7d8a7f954c144ab7055a4d1ceb56da08",
+  WXAPP_EPC_APP_ID = "wxbbdb916ee0ef18aa",
+  WXAPP_EPC_APP_SECRET = "7d8a7f954c144ab7055a4d1ceb56da08",
   PORT = 8888
 } = process.env;
 
